d482d0614aaa95e69ae3a7f03fe7a8ab280e73e0
viollenaki <tanakaakuustik@gmail.com>
2025-03-22 13:16:41 +0600

    update

--- lib/pages/ReportsScreen.dart
+++ lib/pages/ReportsScreen.dart
@@ -446,7 +446,7 @@ class _ReportsScreenState extends State<ReportsScreen> {
     return DropdownButtonFormField<String>(
       value: selectedType,
       decoration: InputDecoration(
-        labelText: "Typeee",
+        labelText: "Tyypeee",
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8),
         ),
