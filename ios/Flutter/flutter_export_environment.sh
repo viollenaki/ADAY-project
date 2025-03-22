a72d7fc4b3d7f128f6c5757a39a8bbaa3ec3943b
Dair12 <dair.aynakulov@gmail.com>
2025-03-22 16:00:33 +0600

    Рабочая версия

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\akbar\Desktop\ADAY project\Personal_Finance_App"
+export "FLUTTER_ROOT=D:\Program\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\RealProject\Personal_Finance_App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
